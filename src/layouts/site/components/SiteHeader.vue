e88cc4f7609ac5a98c8084b574d2f5ab8b71c1d5
Chris Sevilleja <chris@scotch.io>
2019-02-14 11:33:46 -0800

    navbar work

--- src/layouts/site/components/SiteHeader.vue
+++ src/layouts/site/components/SiteHeader.vue
@@ -1,8 +1,23 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+  <nav id="nav" class="navbar is-dark">
+    <div class="container">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item">Home</router-link>
+          <router-link to="/about" class="navbar-item">About</router-link>
+        </div>
+        <div class="navbar-end">
+          <form>
+            <input type="search" class="input">
+          </form>
+        </div>
+      </div>
     </div>
-  </div>
+  </nav>
 </template>
+
+<style lang="scss" scoped>
+.input {
+  background: #333;
+}
+</style>
