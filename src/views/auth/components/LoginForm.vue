94726b35c226ed77a43b85fc7a2f79f33ec75c90
Chris Sevilleja <chris@scotch.io>
2019-02-15 22:11:18 -0800

    starting login forms

---
+++ src/views/auth/components/LoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form :submit="submitForm">
+      <!-- email -->
+      <div class="field">
+        <label for class="label"></label>
+        <input type="email" class="input">
+
+        <p class="help is-error" v-if="$v.email.dirty && $v.email.invalid">Email is required.</p>
+      </div>
+
+      <!-- password -->
+      <div class="field">
+        <label for class="label"></label>
+        <input type="email" class="input">
+
+        <p
+          class="help is-error"
+          v-if="$v.password.dirty && $v.password.invalid"
+        >Password is required and must be 6 characters long.</p>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, minLength } from "vuelidate/lib/validators";
+
+export default {
+  mixins: [validationMixin],
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  validations: {
+    email: {
+      required
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
